36b8a9b40ec48fe1ed981cde1298665abe38befc
HeavenlyBerserker <hxu.yunocreative@gmail.com>
2024-05-02 22:38:47 -0600

    Added project files. Followed tutorial and replicated code and run.

---
+++ diffusion.py
@@ -0,0 +1,33 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+
+def show_images(datset, num_samples=20, cols=4):
+    """ Plots some samples from the dataset """
+    plt.figure(figsize=(15,15)) 
+    for i, img in enumerate(data):
+        if i == num_samples:
+            break
+        plt.subplot(int(num_samples/cols) + 1, cols, i + 1)
+        plt.imshow(img[0])
+
+class StanfordCars(torch.utils.data.Dataset):
+    def __init__(self, root_path, transform = None):
+        self.images = [os.path.join(root_path, file) for file in os.listdir(root_path)]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        image_file = self.images[index]
+        image = Image.open(image_file).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+        return image[None]
+
+# data = torchvision.datasets.StanfordCars(root=".", download=True)
+data = StanfordCars("./stanford_cars/")
+show_images(data)
+
+
